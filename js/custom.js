e7e841da2e6718cea5355637712781f6bc9c3371
yykyeong <yykyeong@naver.com>
2023-07-18 18:39:14 +0900

    deal-section

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,76 @@
-$(function(){
+$(function () {
+
+
+
+
+  /* deal_countdown  */
+  $(".deal-countdown")
+  .countdown("2023/12/25", function(event){
+    $(this).html(
+      event.strftime('<span>%DDAY</span> %H : %M : %S')
+    )
+  })
+
+
 
 
   /* visual */
   $('.visual-list').slick({
-    infinite: true, 
+    infinite: true,
     arrows: false,
-    dots: false, 
-    autoplay: true, 
-    autoplaySpeed: 3000, 
-    fade: false, 
-    speed: 300, 
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: false,
+    speed: 300,
     pauseOnHover: true
   });
 
   /* best */
-  $('.prd-list').slick({
+  $('.best-group .prd-list').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true, 
-    autoplaySpeed: 3000, 
+    autoplay: true,
+    autoplaySpeed: 3000,
     arrows: true,
     dots: false,
 
 
-    responsive: [
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+          arrows: false
+        }
+      },
       {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          dots: false,
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  /* new */
+  $('.new-group .prd-list').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    dots: false,
+
+
+    responsive: [{
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
